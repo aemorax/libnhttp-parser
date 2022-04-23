7dc56d04191288e227f27c21a919f2b35ae309ed
nevergarden <nevergarden.fa@gmail.com>
2022-04-23 16:58:55 +0430

    add: full request parsing

--- nhttp-parser.c
+++ nhttp-parser.c
@@ -2,20 +2,13 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <stdio.h>
 
 #define CR 13
 #define LF 10
 #define SP 32
-
-typedef enum parse_status {
-  ERROR_REQUEST_METHOD_MALFORMED = -2,
-  ERROR_REQUEST_MALFORMED = -1,  // for preprocessing request
-  STATUS_OK = 0,
-} n_parse_error_t;
 
 static const int8_t token_map[128] = {
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  // 0-9
@@ -70,37 +63,45 @@ static const int8_t hex_map[128] = {
 
 // [48-57]
 static const int8_t digit_map[128] = {
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
-    1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 0       
-};
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-static const int32_t http_version_parse_machine(const char * buf, const char * endbuf, nhttp_request_t * req) {
+static const uint32_t predict_http_headers_count(const char *buf,
+                                                 const char *endbuff) {
+  uint32_t count = 0;
+
+  while (buf < endbuff) {
+    if (*(buf) == CR && *(buf + 1) == LF) {
+      count++;
+      buf++;
+    }
+    buf++;
+  }
+  return count;
+}
+
+static const int32_t http_version_parse_machine(const char *buf,
+                                                const char *endbuf,
+                                                nhttp_raw_request_t *req) {
   int32_t movement = 0;
   char c = *buf;
   int32_t minor_pos = 0;
 STATE_A:
-  if(digit_map[c]) {
+  if (digit_map[c]) {
     movement++;
   } else {
     return -1;
   }
 STATE_B:
-  c = *(buf+movement);
-  if(digit_map[c]) {
+  c = *(buf + movement);
+  if (digit_map[c]) {
     movement++;
     goto STATE_B;
-  } else if(c == '.') {
+  } else if (c == '.') {
     // Special action to parse version to uint8
     char major[movement];
     memcpy(&major, buf, movement);
@@ -112,25 +113,27 @@ STATE_B:
     return -1;
   }
 STATE_C:
-  c = *(buf+movement);
-  if(digit_map[c]) {
+  c = *(buf + movement);
+  if (digit_map[c]) {
     movement++;
     goto STATE_C;
   }
-  char minor[movement-minor_pos];
-  memcpy(&minor, buf+minor_pos, movement-minor_pos);
+  char minor[movement - minor_pos];
+  memcpy(&minor, buf + minor_pos, movement - minor_pos);
   req->version_minor = atoi(minor);
 
   return movement;
 }
 
-static const n_parse_error_t process_request(const char * buf, const char * endbuf, nhttp_request_t * req) {
+const n_parse_error_t process_raw_request(const char *buf, const char *endbuf,
+                                          nhttp_raw_request_t *req) {
   const char *start_pos = buf;
 
   // Initializing Method
   req->method = buf;
   while (*buf != SP) {
-    if (*buf == CR || buf == endbuf || !token_map[*buf]) return ERROR_REQUEST_MALFORMED;
+    if (*buf == CR || buf == endbuf || !token_map[*buf])
+      return ERROR_REQUEST_MALFORMED;
     buf++;
   }
   req->method_len = buf - start_pos;
@@ -141,13 +144,15 @@ static const n_parse_error_t process_request(const char * buf, const char * endb
   start_pos = buf;
   while (*buf != SP) {
     if (*buf == CR || buf > endbuf) return ERROR_REQUEST_MALFORMED;
-    // Next line is uric check
-#define HEX_CHECK (*buf == '%' && buf+2 <= endbuf && hex_map[*(buf+1)] && hex_map[*(buf+2)])
+      // Next line is uric check
+#define HEX_CHECK                                             \
+  (*buf == '%' && buf + 2 <= endbuf && hex_map[*(buf + 1)] && \
+   hex_map[*(buf + 2)])
     if (reserve_unreserve_map[*buf]) {
       buf++;
-    } else if( HEX_CHECK ) {
-      buf+=3;
-    } else if (*buf!=' ') {
+    } else if (HEX_CHECK) {
+      buf += 3;
+    } else if (*buf != ' ') {
       return ERROR_REQUEST_MALFORMED;
     }
 #undef HEX_CHECK
@@ -160,13 +165,14 @@ static const n_parse_error_t process_request(const char * buf, const char * endb
   start_pos = buf;
 
   // HTTP/d.dCRLF string is at least 9 character length.
-  if(buf + 9 < endbuf && *buf == 'H' && *(buf+1) == 'T' && *(buf+2) == 'T' && *(buf+3) == 'P' && *(buf+4) == '/') {
-    buf+=5;
+  if (buf + 9 < endbuf && *buf == 'H' && *(buf + 1) == 'T' &&
+      *(buf + 2) == 'T' && *(buf + 3) == 'P' && *(buf + 4) == '/') {
+    buf += 5;
     int32_t m = http_version_parse_machine(buf, endbuf, req);
-    if(m==-1) return ERROR_REQUEST_MALFORMED;
+    if (m == -1) return ERROR_REQUEST_MALFORMED;
 
-    buf+=m;
-    if (*(buf) != CR && *(buf +1) != LF) {
+    buf += m;
+    if (*(buf) != CR && *(buf + 1) != LF) {
       return ERROR_REQUEST_MALFORMED;
     }
   } else {
@@ -176,42 +182,34 @@ static const n_parse_error_t process_request(const char * buf, const char * endb
   buf += 2;
 
   // Initializing Request-Headers
-  // req->headers = buf;
+  req->header = buf;
   start_pos = buf;
-  while (buf - 1 <= endbuf) {
-    if (buf > endbuf) return ERROR_REQUEST_MALFORMED;
 
-    if (*buf == CR) {
-      if (*(buf + 1) == LF) {
-        break;
-      } else {
-        return ERROR_REQUEST_MALFORMED;
-      }
-    }
-    buf++;
+STATE_A:
+  if (buf > endbuf) return ERROR_REQUEST_MALFORMED;
+  if (*buf == CR && *(buf + 1) == LF && buf + 1 < endbuf) goto END;
+  buf++;
+STATE_B:
+  if (buf > endbuf) return ERROR_REQUEST_MALFORMED;
+  if (*buf == CR && *(buf + 1) == LF && buf + 1 < endbuf) {
+    buf += 2;
+    goto STATE_A;
   }
-  // req->request_headers_len = buf - start_pos;
+  buf++;
+  goto STATE_B;
+END:
+  req->header_len = buf - start_pos;
   buf += 2;
+
+  req->headers_count =
+      predict_http_headers_count(req->header, req->header + req->header_len);
 
   req->message = buf;
   start_pos = buf;
-  do {
+  while (buf < endbuf) {
     buf++;
-  } while (buf <= endbuf);
-  buf--;
+  }
   req->message_len = buf - start_pos;
-
-  return STATUS_OK;
-}
-
-
-static const n_parse_error_t parse_request(const char *buf, size_t buf_len,
-                                           nhttp_request_t *req) {
-  if (buf_len <= 0) return ERROR_REQUEST_MALFORMED;
-
-  n_parse_error_t parse_error = STATUS_OK;
-  parse_error = process_request(buf, buf+buf_len, req);
-  if (parse_error < 0) return parse_error;
 
   return STATUS_OK;
 }
--- nhttp-parser.h
+++ nhttp-parser.h
@@ -5,19 +5,20 @@
 #ifndef NHTTP_PARSER__H
 #define NHTTP_PARSER__H
 
-#if !defined(__WINDOWS__) && (defined(WIN32) || defined(WIN64) || defined(_MSC_VER) || defined(_WIN32) )
+#if !defined(__WINDOWS__) && \
+    (defined(WIN32) || defined(WIN64) || defined(_MSC_VER) || defined(_WIN32))
 #define __WINDOWS__
 #error "Windows is shit not supported yet"
 #endif
 
-#if (defined (__GNUC__)) && defined(NHTTP_API_VISIBLE)
+#if (defined(__GNUC__)) && defined(NHTTP_API_VISIBLE)
 #define NHTTP_PUBLIC(type) __attribute__((visibility("default"))) type
 #else
 #define NHTTP_PUBLIC(type) type
 #endif
 
-#if defined (__GNUC__) || defined (__clang__) || defined (__TINTC__)
-#define N_PACK_TIGHT __attribute__ ((packed))
+#if defined(__GNUC__) || defined(__clang__) || defined(__TINTC__)
+#define N_PACK_TIGHT __attribute__((packed))
 #else
 #define N_PACK_TIGHT
 #endif
@@ -29,20 +30,26 @@
 #include <stddef.h>
 #include <stdint.h>
 
+typedef enum parse_status {
+  ERROR_REQUEST_METHOD_MALFORMED = -2,
+  ERROR_REQUEST_MALFORMED = -1,  // for preprocessing request
+  STATUS_OK = 0,
+} n_parse_error_t;
+
 typedef struct N_PACK_TIGHT nhttp_request_header {
-  const char * key;
+  const char* key;
   size_t key_len;
-  const char * value;
+  const char* value;
   size_t value_len;
 } nhttp_request_header_t;
 
-typedef struct N_PACK_TIGHT nhttp_request {
+typedef struct N_PACK_TIGHT nhttp_raw_request {
   // Method
-  const char * method;
+  const char* method;
   size_t method_len;
-  
+
   // URI
-  const char * uri;
+  const char* uri;
   size_t uri_len;
 
   // Version
@@ -50,14 +57,17 @@ typedef struct N_PACK_TIGHT nhttp_request {
   uint8_t version_minor;
 
   // Headers
-  const nhttp_request_header_t * headers;
-  size_t headers_len;
+  const char* header;
+  size_t header_len;
+  size_t headers_count;
 
   // Message
-  const char * message;
+  const char* message;
   size_t message_len;
-} nhttp_request_t;
+} nhttp_raw_request_t;
 
 #undef N_PACK_TIGHT
+const n_parse_error_t process_raw_request(const char* buf, const char* endbuf,
+                                          nhttp_raw_request_t* req);
 
 #endif
